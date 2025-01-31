273b9388699af1c0b6716c9fab5d1582acb7a0c8
Cole Oliva <olivac2@wwu.edu>
2025-01-31 12:20:44 -0800

    literally barebones website. has two sentences in html plaintext. don't expect much

---
+++ frontend-barebones/testing-website/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="testing-app">
+      <header className="site-header">hey gang.</header>
+      well. at least we know this works.
+    </div>
+  );
+}
+
+export default App;
