e7095df435640d63b842dd16e3be29363439773a
Alex Lo <alexhongxilo@gmail.com>
2025-03-02 20:42:21 -0800

    Added API abstract info

---
+++ PubmedAPIFiles/MetapubTest.py
@@ -0,0 +1,36 @@
+import requests
+from metapub import PubMedFetcher
+from metapub.findit import FindIt
+from pypdf import PdfReader
+
+# Dependencies: pypdf and metapub
+# Run program and it should pull the paper and download the pdf and print the text
+# of the first page into a txt file
+def main():
+    fetch = PubMedFetcher()
+    # article = fetch.article_by_pmid('29732299')
+    src = FindIt(doi='10.1542/peds.2008-3782')
+    #src = FindIt("22993294")
+    if src.url is None: print("Reason: "+src.reason)
+    else:
+        url = src.url
+        response = requests.get(url)
+        file_Path = 'test_paper.pdf'
+
+        if response.status_code == 200:
+            with open(file_Path, 'wb') as file:
+                file.write(response.content)
+            print('File downloaded successfully')
+        else:
+            print('Failed to download file')
+
+        reader = PdfReader("test_paper.pdf")
+        page = reader.pages[0]
+        text = page.extract_text()
+        file = open("test_paper_pg_1.txt", "w")
+        file.write(text)
+        file.close()
+    
+
+if __name__ == "__main__":
+    main()
---
+++ PubmedAPIFiles/pubmed_api_in_python_2024.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import json
+from Bio import Entrez
+from pathlib import Path
+'''
+Things I removed and should maybe remember:
+- Full queries
+- [Title/Abstract] and [Author]
+'''
+# Set the email address to avoid any potential issues with Entrez
+Entrez.email = 'loa4@wwu.edu'
+Entrez.api_key = 'c301edeca095efe481ce5e2a727560444908'
+
+# May need to adjust path depending on what directory you run this in
+path = str(Path.cwd()) + '/PubmedAPI/Harle - ASD bio tx .xlsx'
+#print(path)
+
+oldDf = pd.read_excel(path, 'Tx Data', usecols="A,D")
+
+# Define lists of authors and topics
+
+authors = oldDf['First Author'].tolist() # Example authors, adjust as needed
+topics = oldDf['Treatment name'].tolist()  # Example topics, adjust as needed
+
+# Define date range
+#date_range = '("2012/03/01"[Date - Create] : "2022/12/31"[Date - Create])'
+
+# Build the query dynamically based on the available authors and topics
+queries = []
+author_queries = []
+topic_queries = []
+
+if authors:
+    #author_queries = ['{}[Author]'.format(author) for author in authors]
+    author_queries = ['{}'.format(author) for author in authors]
+    #print(author_queries)
+    #queries.append('(' + ' OR '.join(author_queries) + ')')
+
+if topics:
+    for treatment in topics:
+        if 'and' in treatment:
+            treatment.replace('and', '')
+    topic_queries = ['{}'.format(topic) for topic in topics]
+    #queries.append('(' + ' OR '.join(topic_queries) + ')')
+
+for i in range(len(author_queries)):
+    queries.append(author_queries[i] + ' AND ' + topic_queries[i])
+
+'''
+print(author_queries)
+print(topic_queries)
+print(queries)
+'''
+completePD = pd.DataFrame()
+
+for query in queries:
+    # Search PubMed for relevant records
+    handle = Entrez.esearch(db='pubmed', retmax=10, term=query)
+    record = Entrez.read(handle)
+    id_list = record['IdList']
+
+    # DataFrame to store the extracted data
+    df = pd.DataFrame(columns=['PMID', 'Title', 'Abstract', 'Authors', 'Journal', 'Keywords', 'URL', 'Affiliations'])
+
+    # Fetch information for each record in the id_list
+    for pmid in id_list:
+        handle = Entrez.efetch(db='pubmed', id=pmid, retmode='xml')
+        records = Entrez.read(handle)
+
+        # Process each PubMed article in the response
+        for record in records['PubmedArticle']:
+            # Print the record in a formatted JSON style
+            #print(json.dumps(record, indent=4, default=str))  # default=str handles types JSON can't serialize like datetime
+            title = record['MedlineCitation']['Article']['ArticleTitle']
+            abstract = ' '.join(record['MedlineCitation']['Article']['Abstract']['AbstractText']) if 'Abstract' in record['MedlineCitation']['Article'] and 'AbstractText' in record['MedlineCitation']['Article']['Abstract'] else ''
+            authors = ', '.join(author.get('LastName', '') + ' ' + author.get('ForeName', '') for author in record['MedlineCitation']['Article']['AuthorList'])
+            
+            affiliations = []
+            for author in record['MedlineCitation']['Article']['AuthorList']:
+                if 'AffiliationInfo' in author and author['AffiliationInfo']:
+                    affiliations.append(author['AffiliationInfo'][0]['Affiliation'])
+            affiliations = '; '.join(set(affiliations))
+
+            journal = record['MedlineCitation']['Article']['Journal']['Title']
+            keywords = ', '.join(keyword['DescriptorName'] for keyword in record['MedlineCitation']['MeshHeadingList']) if 'MeshHeadingList' in record['MedlineCitation'] else ''
+            url = f"https://www.ncbi.nlm.nih.gov/pubmed/{pmid}"
+
+            new_row = pd.DataFrame({
+                'PMID': [pmid],
+                'Title': [title],
+                'Abstract': [abstract],
+                'Authors': [authors],
+                'Journal': [journal],
+                'Keywords': [keywords],
+                'URL': [url],
+                'Affiliations': [affiliations]
+            })
+
+            df = pd.concat([df, new_row], ignore_index=True)
+    completePD = pd.concat([completePD, df], ignore_index=True)
+    print("Finished " + query)
+
+# Save DataFrame to an Excel file
+completePD.to_excel('Adjusted_ASD_Sheet_Info.xlsx', index=False)
+
