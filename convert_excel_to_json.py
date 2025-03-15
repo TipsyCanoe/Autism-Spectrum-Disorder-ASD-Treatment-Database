fd97ad7baf528f50dbbc24da30c871e95025a361
Alex Lo <alexhongxilo@gmail.com>
2025-03-14 22:57:49 -0700

    Removed old files

--- convert_excel_to_json.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-# Use: Add name of excel file in string with path, and sheet name in oldDf string
-path = str(Path.cwd()) + '/Adjusted_ASD_Sheet_Info_V2.xlsx'
-oldDf = pd.read_excel(path, 'Sheet1')
-
-oldDf.to_json("Adjusted_ASD_Sheet_Info_V2.json")
-
