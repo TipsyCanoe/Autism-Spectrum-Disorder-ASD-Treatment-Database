cfd3abc06b5b86c2dc30a10500d6a2e8d12e83b6
Alex Lo <alexhongxilo@gmail.com>
2025-05-28 15:23:41 -0700

    Updated the convert excel script to match new file names

--- PubmedAPIFiles/convert_excel_to_json.py
+++ PubmedAPIFiles/convert_excel_to_json.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pathlib import Path
 
 # Use: Add name of excel file in string with path, and sheet name in oldDf string
-file_heads = ['pubmed_papers_info.xlsx', 'Adjusted_ASD_Sheet_Info_V2.xlsx', 'Individual_Treatment_Queries.xlsx']
+file_heads = ['pubmed_papers_info.xlsx', 'pubmed_treatment_info.xlsx', 'pubmed_ASD_info.xlsx']
 
 for file_head in file_heads:
     path = str(Path.cwd()) + '/' + file_head
