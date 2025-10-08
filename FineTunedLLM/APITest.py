d0655d9e88ec2140a91d118f5ef1fd5fdf8ec812
kallowl <kallowl@wwu.edu>
2025-10-07 21:01:42 -0700

    added starter API code

---
+++ FineTunedLLM/APITest.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+client = OpenAI()
+
+batch_input_file = client.files.create(
+    file=open("batchinput.jsonl", "rb"),
+    purpose="batch"
+)
+
+print(batch_input_file)
+
+batch_input_file_id = batch_input_file.id
+client.batches.create(
+    input_file_id=batch_input_file_id,
+    endpoint="/v1/chat/completions",
+    completion_window="24h",
+    metadata={
+        "description": "batch 1 test"
+    }
+)
